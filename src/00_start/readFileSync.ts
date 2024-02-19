c05684231d409fefd4b5edd59efdc4c417158717
kim-elijah-sol <kimelijahsol@gmail.com>
2024-02-19 23:42:17 +0900

    00_start: readFileSync 주석 추가

--- src/00_start/readFileSync.ts
+++ src/00_start/readFileSync.ts
@@ -4,7 +4,10 @@ import * as fs from "fs";
 export class ReadFileSyncError extends Error {}
 
 /**
+ * readFileSync 함수는 실패하는 경우도 존재하고,
+ * 부수 효과도 있는 함수이므로 IO와 Either를 감싸서 반환합니다.
  *
+ * readFileSync 로 파일 읽기에 성공 했을 때 파일의 내용을 반환하므로 string 타입을 사용했습니다.
  */
 export function readFileSync(path: string): IOEither<Error, string> {
   return tryCatch(
