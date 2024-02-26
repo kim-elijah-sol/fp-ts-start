77309c2010cce4abc6c2bfb3ee5aa4e71f2e8024
kim-elijah-sol <kimelijahsol@gmail.com>
2024-02-26 12:06:56 +0900

    02_Ord: ordNumber 주석 추가

--- src/02_Ord/ordNumber.ts
+++ src/02_Ord/ordNumber.ts
@@ -1,5 +1,13 @@
 import { Ord } from "./Ord";
 
+/**
+ * ordNumber 인스턴스는 Ord 타입 클래스의 인스턴스로, 숫자를 정렬할 때 사용합니다.
+ *
+ * 인스턴스는 아래 3가지 규칙을 만족합니다.
+ * - `반사성 ( Reflexivity )` : 모든 `x` 에 대해 `compare(x, x) === 0` 를 만족합니다.
+ * - `대칭성 ( Antisymmetry )` : 모든 `x`, `y` 에 대해 `compare(x, y) === compare(y, x)` 를 만족합니다.
+ * - `이동성 ( Transitivity )` : 모든 `x`, `y`, `z` 에 대해 `compare(x, y) === 0` 이고 `compare(y, z) === 0` 이면 `compare(x, z) === 0` 을 만족합니다.
+ */
 export const ordNumber: Ord<number> = {
   equals: (x, y) => x === y,
   compare: (x, y) => (x < y ? -1 : x > y ? 1 : 0),
