0ffd3a9b6c417774139bc5ffd23f2a8c880dd42b
kim-elijah-sol <kimelijahsol@gmail.com>
2024-03-21 14:18:04 +0900

    10_IO: randomBool 주석 추가

--- src/10_IO/randomBool.ts
+++ src/10_IO/randomBool.ts
@@ -2,4 +2,9 @@ import { map } from "fp-ts/lib/IO";
 import { IO } from "./IO";
 import { random } from "./random";
 
+/**
+ * `IO` 는 `Monad` 인스턴스를 허용하기에 `map` 을 사용할 수 있습니다.
+ *
+ * `map: <A, B>(f: (a: A) => B): (fa: IO<A>) => IO<B>`
+ */
 export const randomBool: IO<boolean> = map((n: number) => n > 0.5)(random);
